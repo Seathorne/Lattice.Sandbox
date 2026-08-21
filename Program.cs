33734ea00b3d4d1372ffa8a5b84df75bcf2d5e28
Scott Clarke <scottketclarke@gmail.com>
2026-08-21 08:33:25 -0400

    Call Terminal.RunDiagnostics() as Sandbox entry point
    Tests new diagnostic run mode which probes Unicode tables for character widths.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using System.Diagnostics;
+using Lattice.Text;
+
 namespace Lattice.Sandbox;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        new Lattice.Terminal().Run();
+        Trace.Listeners.Add(new TextWriterTraceListener("lattice-trace.log"));
+        Trace.AutoFlush = true;
+        
+        new Lattice.Terminal().RunDiagnostics([ProbeRanges.MahjongTiles]);
     }
 }
