f6aaf1ee9017dc1326e4997bf0f3304eafc24132
Scott Clarke <scottketclarke@gmail.com>
2026-08-24 21:13:40 -0400

    Improve demo screen
    Added TestScreen._status to prove each button activates independently.

--- Screens/TestScreen.cs
+++ Screens/TestScreen.cs
@@ -5,20 +5,32 @@ namespace Lattice.Sandbox.Screens;
 
 public sealed class TestScreen : Screen
 {
-    [AdaptiveWidth]
+    [FillWidth]
     private ButtonElement _hello = null!;
 
     [FillWidth]
     private ButtonElement _world = null!;
 
+    [FillWidth]
+    private TextElement _status = null!;
+
     protected override Element Build()
     {
         _hello = new ButtonElement { Label = "Hello" };
         _world = new ButtonElement { Label = "World" };
+        _status = new TextElement { Text = "ready" };
 
-        GridElement root = GridElement.Row(2);
-        root.AddChild(_hello);
-        root.AddChild(_world);
+        _hello.OnActivate = () => _status.Text = "hello pressed.";
+        _world.OnActivate = () => _status.Text = "world pressed.";
+
+        GridElement root = GridElement.Column(2);
+        GridElement buttons = GridElement.Row(2);
+
+        buttons.AddChild(_hello);
+        buttons.AddChild(_world);
+
+        root.AddChild(buttons);
+        root.AddChild(_status);
 
         return root;
     }
